a12c609b6f2a0ef113f07301594121e6ebe5d8fe
Xilin Sun <s.sn.giraffe@gmail.com>
2022-10-17 11:42:45 -0700

    Enable 12 hour mode

--- xiaoClock/background.js
+++ xiaoClock/background.js
@@ -36,32 +36,39 @@ browser.browserAction.onClicked.addListener(() => {
   store.dispatch({type: 'CHANGE'})
 })
 
-const colors = ["white", "grey", "black"]
+const colorArray = ["white", "grey", "black"]
+const colors = colorArray.concat(colorArray)
+const hours = Array(colorArray.length).fill(true).concat(Array(colorArray.length).fill(false))
 
 // sets the icon and title
 const render = () => {
   const colorIndex = store.getState().colorIndex
   const color = colors[colorIndex]
-  console.log("Current colorIndex is " + colorIndex)
+  const hour12 = hours[colorIndex]
+  // console.log("Current colorIndex is " + colorIndex)
 
   const date = new Date()
   const dateString = date.toLocaleString(
     'en-US', {
       hour: 'numeric',
       minute: 'numeric',
-      hour12: false
+      hour12
     })
+  // console.log(dateString)
   const hr = dateString.slice(0, 2)
   const mn = dateString.slice(3, 5)
+  const ampm = dateString.slice(6, 7)
 
   // generates the image that contains current time
   const canvas = document.createElement("canvas")
   const context = canvas.getContext("2d")
   context.fillStyle = color
-  context.font = "bold 80px Verdana"
+  context.font = "bold 78px Verdana"
   context.fillText(hr, 8, 64)
-  context.font = "bold 80px Verdana"
+  context.font = "bold 78px Verdana"
   context.fillText(mn, 8, 128)
+  context.font = "bold 36px Verdana"
+  context.fillText(ampm, 100, 128)
   const imageData = context.getImageData(0, 0, 128, 128)
   browser.browserAction.setIcon({imageData: imageData})
   browser.browserAction.setTitle({title: date.toISOString().slice(0,10)})
